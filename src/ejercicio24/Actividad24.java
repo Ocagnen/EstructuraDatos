5d8ca4dc94cc6a7dcd5730b6a37149044208692d
Javier <ocagnen2@gmail.com>
2019-02-22 12:31:37 +0100

    Correccion actividad 24

--- src/ejercicio24/Actividad24.java
+++ src/ejercicio24/Actividad24.java
@@ -13,50 +13,30 @@ import java.util.Random;
  * @author Javier
  */
 public class Actividad24 {
-    
+
     public static void main(String[] args) {
-        
+
         Random alt = new Random();
-        
+
         double[] a = new double[20];
-        
+
         Arrays.fill(a, 7.5);
-        
-        double[] b = new double[20];
-        
-        for (int i = 0; i < b.length; i++) {
-            b[i]=a[i];
-        }
-        
-        System.out.println("");
-        for (double d : a) {
-            System.out.println(d);
-        }
-        
-        System.out.println("");
-        for (double d : b) {
-            System.out.println(d);
-        }
-        
+
+        double[] b = Arrays.copyOf(a, 20);
+
+        Arrays.toString(a);
+        Arrays.toString(b);
+
         System.out.println(Arrays.equals(a, b));
+
+        a[alt.nextInt(a.length)] = 6.3;
+
+        Arrays.toString(a);
+        Arrays.toString(b);
         
-        a[alt.nextInt(a.length)]=6.3;
-        
-        System.out.println("");
-        for (double d : a) {
-            System.out.println(d);
-        }
-        
-        System.out.println("");
-        for (double d : b) {
-            System.out.println(d);
-        }
-        
+
         System.out.println(Arrays.equals(a, b));
-        
-               
-        
-        
+
     }
-    
+
 }
