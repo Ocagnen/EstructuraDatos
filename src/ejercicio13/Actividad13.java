0ab4ef33a371a2c7107b6b5df886e49be6b40861
Javier <ocagnen2@gmail.com>
2019-03-05 14:34:43 +0100

    Actividad13

---
+++ src/ejercicio13/Actividad13.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio13;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author javier
+ */
+public class Actividad13 {
+
+    private static double[][] matriz = new double[3][3];
+
+    public static double[][] getMatriz() {
+        return matriz;
+    }
+
+    public static void setMatriz(double[][] matriz) {
+        Actividad13.matriz = matriz;
+    }
+
+    public static void rellenarMatriz() {
+        Scanner tec = new Scanner(System.in);
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.println("Introduce un número para la posición " + j + "del array de la fila " + i);
+                matriz[i][j] = tec.nextDouble();
+            }
+        }
+    }
+
+    public static double calcularMedia() {
+
+        double sumaTotal = 0;
+        int valoresTotales = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                sumaTotal = sumaTotal + matriz[i][j];
+                valoresTotales++;
+            }
+
+        }
+
+        return sumaTotal / valoresTotales;
+    }
+
+    public static double calcularMin() {
+        
+        
+        double[] arrayP = new double[matriz.length];
+
+        for (int i = 0; i < matriz.length; i++) {            
+                Arrays.sort(matriz[i]);
+                arrayP[i]=matriz[i][0];    
+            }
+        Arrays.sort(arrayP);
+        
+        return arrayP[0];  
+        
+
+    }
+    
+    public static double calcularMax() {
+        
+        double[] arrayP = new double[matriz.length];
+
+        for (int i = 0; i < matriz.length; i++) {            
+                Arrays.sort(matriz[i]);
+                arrayP[i]=matriz[i][matriz[i].length-1];    
+            }
+        Arrays.sort(arrayP);
+        
+        return arrayP[arrayP.length-1];  
+        
+
+    }
+    
+    
+
+}
---
+++ src/ejercicio13/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio13;
+
+/**
+ *
+ * @author javier
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Actividad13.rellenarMatriz();
+        System.out.println("La media es "+Actividad13.calcularMedia());
+        System.out.println("El minimo es "+Actividad13.calcularMin());
+        System.out.println("El max es "+Actividad13.calcularMax());
+        
+    }
+    
+}
